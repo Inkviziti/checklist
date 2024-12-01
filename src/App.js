cc68141a584f6f52b2f73512496fdfbeb20173ba
Sergey Lopaev <sv.lopaev@mail.ru>
2024-12-01 19:04:14 +0500

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 
+//Определение функции приложения
 function App() {
+  //переменная "task" и функция "setTasks" для ее обновления.
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState('');
 
+  //функция для добавления новой задачи
   const addTask = () => {
+    //проверка, что задача не пуста
     if (task.trim()) {
+      //добавление новой задачи
       setTasks([...tasks, { text: task, completed: false }]);
+      //очистка поле ввода 
       setTask('');
     }
   };
 
 
+  //Функция отмечена или нет, по ее порядковому номеру
   const toggleTask = (index) => {
     setTasks(tasks.map((t, i) => i === index ? { ...t, completed: !t.completed } : t));
   };
 
+  //return() Все что в скобках будет отображаться на экране
+    //Создаем контейнер, отступ и шрифт
+    //input - Поле ввода текста для добавления задачи
+    //ul - список задача, il - отдельная задача
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
       <h1>Checklist</h1>
